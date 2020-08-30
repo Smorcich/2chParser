8e6c46282fc7eb95fa3ce8f3b82cb06b1479e584
Smorcich <52823878+Smorcich@users.noreply.github.com>
2020-08-30 21:21:36 +0300

    #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     private static Document getPage() throws IOException {
@@ -11,7 +14,28 @@ public class Main {
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     }
-    public static void main(String[] args) throws IOException {
-        System.out.println(getPage());
+
+    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString(String date) throws Exception {
+        Matcher matcher = pattern.matcher(date);
+        if(matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Cant extract data from string");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document page = getPage();
+        //css querry lang
+        Element tableWeather = page.select("table[class=wt]").first();
+        Elements names = tableWeather.select("tr[class=wth]");
+        Elements values = tableWeather.select("tr[valign=top]");
+        //System.out.println(names);
+        for (Element name: names) {
+            String dateString = name.select("th[id=dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date);
+        }
     }
 }
